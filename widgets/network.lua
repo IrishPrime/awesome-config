3ae42e9955cc5f894a4e023cdfce5852a8246d1f
Michael O'Neill <irish.dot@gmail.com>
2013-07-15 11:12:19 -0400

    Change formatting in network widget.

--- widgets/network.lua
+++ widgets/network.lua
@@ -5,36 +5,36 @@ local vicious   = require("vicious")
 local M = {}
 
 M.downicon = wibox.widget.imagebox()
-M.downicon:set_image(beautiful.widget_netd)
+M.downicon:set_image(beautiful.widget_net_down)
 
 M.upicon = wibox.widget.imagebox()
-M.upicon:set_image(beautiful.widget_netu)
+M.upicon:set_image(beautiful.widget_net_up)
 
 M.downwidget = wibox.widget.textbox()
 vicious.register(M.downwidget, function(format, warg)
-    local args = vicious.widgets.net(format, warg)
-    local down  = 0
-    for key,value in pairs(args) do
-        if string.find(key, 'down_kb') ~= nil then
-            down = down + args[key]
-        end
-    end
-    args['{down}'] = string.format("%.2fk", down)
-    return args
+	local args = vicious.widgets.net(format, warg)
+	local down  = 0
+	for key,value in pairs(args) do
+		if string.find(key, 'down_kb') ~= nil then
+			down = down + args[key]
+		end
+	end
+	args['{down}'] = string.format("%.2fk", down)
+	return args
 end, "<span>${down}</span>", 1)
 vicious.cache(M.downwidget)
 
 M.upwidget = wibox.widget.textbox()
 vicious.register(M.upwidget, function(format, warg)
-    local args = vicious.widgets.net(format, warg)
-    local up  = 0
-    for key,value in pairs(args) do
-        if string.find(key, 'up_kb') ~= nil then
-            up = up + args[key]
-        end
-    end
-    args['{up}'] = string.format("%.2fk", up)
-    return args
+	local args = vicious.widgets.net(format, warg)
+	local up  = 0
+	for key,value in pairs(args) do
+		if string.find(key, 'up_kb') ~= nil then
+			up = up + args[key]
+		end
+	end
+	args['{up}'] = string.format("%.2fk", up)
+	return args
 end, "<span>${up}</span>", 1)
 vicious.cache(M.upwidget)
 
